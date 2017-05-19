ba62a5a23dba923e785c9a5440333cb00bc7215f
svmir <kanopu1@mail.ru>
2017-05-19 10:43:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+jQuery(function($) {
+
+	//#main-slider
+	var slideHeight = $(window).height();
+	$('#home-transition-slider .item').css('height',slideHeight);
+
+	$(window).resize(function(){'use strict',
+		$('#home-transition-slider .item').css('height',slideHeight);
+	});
+	
+	//Scroll Menu
+	$(window).on('scroll', function(){
+		if( $(window).scrollTop()>100 ){
+			$('.navbar-default').addClass('navbar-fixed-top');
+		} else {
+			$('.navbar-default').removeClass('navbar-fixed-top');
+		}
+	});
+	//Countdown
+	$('#counter').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$(this).find('.timer').each(function () {
+				var $this = $(this);
+				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+					duration: 2000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.ceil(this.Counter));
+					}
+				});
+			});
+			$(this).unbind('inview');
+		}
+	});
+	
+	
+	// Navigation Scroll
+	$(window).scroll(function(event) {
+		Scroll();
+	});
+
+	$('.navbar-collapse ul li a').on('click', function() {  
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		return false;
+	});
+			
+	// User define function
+	function Scroll() {
+		var contentTop      =   [];
+		var contentBottom   =   [];
+		var winTop      =   $(window).scrollTop();
+		var rangeTop    =   200;
+		var rangeBottom =   500;
+		$('.navbar-collapse').find('.scroll a').each(function(){
+			contentTop.push( $( $(this).attr('href') ).offset().top);
+			contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+		})
+		$.each( contentTop, function(i){
+			if ( winTop > contentTop[i] - rangeTop ){
+				$('.navbar-collapse li.scroll')
+				.removeClass('active')
+				.eq(i).addClass('active');			
+			}
+		})
+	};
+});
+
